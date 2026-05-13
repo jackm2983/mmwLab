b9b2a30e912f1181c383b3b4b8a629044cce39c7
jackm2983 <j_master101@hotmail.com>
2026-05-12 20:18:46 -0700

    fixed a motor rotation bug

--- STM32/mmwLab_stm32/src/app/app_mode_cap.c
+++ STM32/mmwLab_stm32/src/app/app_mode_cap.c
@@ -1,10 +1,3 @@
-/**
-  ******************************************************************************
-  * @file           : app_mode_cap.c
-  * @brief          : Capture mode control. Characterizing the antenna by received transmission power.
-  ******************************************************************************
-*/
-
 #include "main.h"
 #include "cfg_sys.h"
 #include "drv_cmd.h"
@@ -15,38 +8,59 @@
 
 typedef enum {
     CAP_INIT,
-    CAP_ROTATE,
+    CAP_ROTATE_AXIS1,
     CAP_COLLECT,
     CAP_SEND,
+    CAP_ROTATE_AXIS2,
     CAP_DONE
 } CapState_t;
 
 static CapState_t cap_state = CAP_INIT;
-static uint32_t cap_step_count = 0;
-static int32_t cap_position = 0;
-static int32_t cap_max_steps = 72;
 
-#define CAP_ROTATION_SPEED  100
+static uint32_t cap_axis1_step_count = 0;
+static uint32_t cap_axis2_step_count = 0;
+
+static int32_t cap_axis1_position = 0;
+static int32_t cap_axis2_position = 0;
+
+#define CAP_ROTATION_SPEED      100
+#define CAP_STEP_DEG            5
+
+#define CAP_AXIS1_MAX_DEG       360
+#define CAP_AXIS2_MAX_DEG       90
+
+#define CAP_AXIS1_STEPS         (CAP_AXIS1_MAX_DEG / CAP_STEP_DEG)
+#define CAP_AXIS2_STEPS         (CAP_AXIS2_MAX_DEG / CAP_STEP_DEG)
 
 void app_mode_cap_update(void)
-
-
-// this needs to be changed so that it rotates both motor1 and motor2. first does a complete sweep of motor1. then tilts motor 2 5'(degrees)
-// , the motor 1 again, then motor
-// 2 5' then motor1, and so on until motor 2 has completed a full sweep at 90' from its start angle.
-
 {
     switch (cap_state)
     {
         case CAP_INIT:
-            drv_cmd_send_status("CAP: Starting sweep");
-            cap_step_count = 0;
-            cap_position = 0;
-            mot_ctrl_move_axis1_abs(cap_position, CAP_ROTATION_SPEED);
-            cap_state = CAP_ROTATE;
+            drv_cmd_send_status("CAP: Starting 2-axis sweep");
+
+            cap_axis1_step_count = 0;
+            cap_axis2_step_count = 0;
+
+            cap_axis1_position = 0;
+            cap_axis2_position = 0;
+
+            mot_ctrl_move_axis2_abs(cap_axis2_position, CAP_ROTATION_SPEED);
+            cap_state = CAP_ROTATE_AXIS2;
             break;
 
-        case CAP_ROTATE:
+        case CAP_ROTATE_AXIS2:
+            if (!mot_axis2_is_moving())
+            {
+                cap_axis1_step_count = 0;
+                cap_axis1_position = 0;
+
+                mot_ctrl_move_axis1_abs(cap_axis1_position, CAP_ROTATION_SPEED);
+                cap_state = CAP_ROTATE_AXIS1;
+            }
+            break;
+
+        case CAP_ROTATE_AXIS1:
             if (!mot_axis1_is_moving())
             {
                 bsp_adc_sampling_reset();
@@ -64,17 +78,34 @@ void app_mode_cap_update(void)
             break;
 
         case CAP_SEND:
-            cap_step_count++;
-            if (cap_step_count < cap_max_steps)
+            /*
+             * send or store captured data here.
+             * include axis1 and axis2 positions if needed.
+             */
+
+            cap_axis1_step_count++;
+
+            if (cap_axis1_step_count < CAP_AXIS1_STEPS)
             {
-                cap_position += 5;
-                mot_ctrl_move_axis1_abs(cap_position, CAP_ROTATION_SPEED);
-                cap_state = CAP_ROTATE;
+                cap_axis1_position += CAP_STEP_DEG;
+                mot_ctrl_move_axis1_abs(cap_axis1_position, CAP_ROTATION_SPEED);
+                cap_state = CAP_ROTATE_AXIS1;
             }
             else
             {
-                cap_state = CAP_DONE;
-                drv_cmd_send_status("CAP: Sweep complete");
+                cap_axis2_step_count++;
+
+                if (cap_axis2_step_count <= CAP_AXIS2_STEPS)
+                {
+                    cap_axis2_position += CAP_STEP_DEG;
+                    mot_ctrl_move_axis2_abs(cap_axis2_position, CAP_ROTATION_SPEED);
+                    cap_state = CAP_ROTATE_AXIS2;
+                }
+                else
+                {
+                    drv_cmd_send_status("CAP: 2-axis sweep complete");
+                    cap_state = CAP_DONE;
+                }
             }
             break;
 
@@ -82,6 +113,7 @@ void app_mode_cap_update(void)
             break;
 
         default:
+            cap_state = CAP_DONE;
             break;
     }
 }
@@ -89,7 +121,10 @@ void app_mode_cap_update(void)
 void app_mode_cap_reset(void)
 {
     cap_state = CAP_INIT;
-    cap_step_count = 0;
+
+    cap_axis1_step_count = 0;
+    cap_axis2_step_count = 0;
+
+    cap_axis1_position = 0;
+    cap_axis2_position = 0;
 }
-
-
