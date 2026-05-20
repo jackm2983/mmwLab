5f0f5bc0907f72645a7e7838d183f14b25e8cd4e
jackm2983 <j_master101@hotmail.com>
2026-05-20 09:22:39 -0700

    decode

--- STM32/mmwLab_stm32/src/app/app_mode_dec.c
+++ STM32/mmwLab_stm32/src/app/app_mode_dec.c
@@ -1,7 +1,7 @@
 /**
   ******************************************************************************
   * @file           : app_mode_dec.c
-  * @brief          : Decode I and Q samples
+  * @brief          : Continuous I/Q read and emit, no movement
   ******************************************************************************
 */
 
@@ -9,6 +9,9 @@
 #include "cfg_sys.h"
 #include "drv_cmd.h"
 #include "bsp_adc.h"
+#include "bsp_uart.h"
+
+#define DEC_INTERVAL_MS     100
 
 typedef enum {
     DEC_INIT,
@@ -18,33 +21,65 @@ typedef enum {
 
 static DecState_t dec_state = DEC_INIT;
 static uint32_t dec_sample_count = 0;
+static uint32_t dec_last_emit = 0;
+
+static void send_int(int32_t v)
+{
+    if (v < 0) { bsp_uart_send_char('-'); v = -v; }
+    char buf[12];
+    int i = 0;
+    if (v == 0) { buf[i++] = '0'; }
+    else {
+        char tmp[12];
+        int j = 0;
+        while (v > 0) { tmp[j++] = '0' + (v % 10); v /= 10; }
+        while (j > 0) { buf[i++] = tmp[--j]; }
+    }
+    buf[i] = 0;
+    bsp_uart_send_string(buf);
+}
+
+static void dec_emit_sample(uint16_t i_val, uint16_t q_val)
+{
+    bsp_uart_send_string("dec,");
+    send_int(dec_sample_count);
+    bsp_uart_send_char(',');
+    send_int(i_val);
+    bsp_uart_send_char(',');
+    send_int(q_val);
+    bsp_uart_send_string("\r\n");
+}
 
 void app_mode_dec_update(void)
 {
     switch (dec_state)
     {
         case DEC_INIT:
-            drv_cmd_send_status("DEC: Starting ADC stream");
+            drv_cmd_send_status("DEC: starting I/Q stream");
             dec_sample_count = 0;
-            bsp_adc_sampling_reset();
-            bsp_adc_start_continuous();
+            dec_last_emit = HAL_GetTick();
             dec_state = DEC_STREAMING;
             break;
 
         case DEC_STREAMING:
-            if (bsp_adc_sampling_complete())
-            {
-                drv_cmd_send_status("DEC: Sample buffer full");
-                dec_state = DEC_STOP;
+            if ((HAL_GetTick() - dec_last_emit) >= DEC_INTERVAL_MS) {
+                dec_last_emit = HAL_GetTick();
+
+                uint16_t i_val = 0;
+                uint16_t q_val = 0;
+                bsp_adc_read_single(&i_val, &q_val);
+
+                dec_emit_sample(i_val, q_val);
+                dec_sample_count++;
             }
             break;
 
         case DEC_STOP:
-            bsp_adc_stop();
-            drv_cmd_send_status("DEC: Stream complete");
+            drv_cmd_send_status("DEC: stream complete");
             break;
 
         default:
+            dec_state = DEC_STOP;
             break;
     }
 }
@@ -53,5 +88,5 @@ void app_mode_dec_reset(void)
 {
     dec_state = DEC_INIT;
     dec_sample_count = 0;
-} 
-
+    dec_last_emit = 0;
+}
